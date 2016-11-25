47ef4eb8a138602a6cc94a598cadfc4e9e72349f
Diego Occhi <diegocchi@gmail.com>
2016-11-25 21:36:42 -0200

    pronto

--- js/webServiceCtrl.js
+++ js/webServiceCtrl.js
@@ -8,20 +8,46 @@ angular.module('webServiceApp').controller('webServiceCtrl', function($scope, $h
 	$scope.ip = 'localhost:8080';
 	$scope.servicoSalvar = '/Sorteio/ws/aposta/salvar';
 	$scope.servicoSortear = '/Sorteio/ws/aposta/sortear';
+	$scope.servicoBuscarTodos = '/Sorteio/ws/aposta/buscarTodos';
+	$scope.ganhadores = [];
+	$scope.apostas = [];
+	$scope.aposta = {};
+	$scope.nenhumGanhador = {
+		nome: 'Nenhum Ganhador',
+		numeros: ''
+	}
 
 	$scope.salvar = function(aposta) {
 		aposta.numeros = $scope.numero1+','+$scope.numero2+','+$scope.numero3+','+$scope.numero4+','+$scope.numero5+','+$scope.numero6;
 		$http.post('http://' + $scope.ip + $scope.servicoSalvar, aposta)
 			.success(function(resultado) {
-				delete aposta;
+				delete $scope.aposta;
+			})
+			.error(function(err) {
+				alert(err);
 			});
 	}
 
 	$scope.sortear = function() {
-		$http.get('http://' + $scope.ip + $scope.servicoLerArquivo, {params:{caminhoArquivo:arquivo}})
-			.success(function(aposta) {
-				$scope.ganhador = aposta.nome;
-				$scope.numerosSorteados = aposta.numeros;
+		$http.get('http://' + $scope.ip + $scope.servicoSortear)
+			.success(function(resultado) {
+				$scope.ganhadores = resultado;
+				if ($scope.ganhadores.length == 0) {
+					$scope.ganhadores.push($scope.nenhumGanhador);
+				}
+			})
+			.error(function(err) {
+				alert(err);
+			});
+	}
+
+	$scope.buscarTodos = function() {
+		$http.get('http://' + $scope.ip + $scope.servicoBuscarTodos)
+			.success(function(resultado) {
+				$scope.apostas = resultado;
+			})
+			.error(function(err) {
+				alert(err);
 			});
 	}
 });
